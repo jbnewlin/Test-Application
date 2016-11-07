eccb7df48c7408396fa4fe5d3a6b46fb13644fe6
Josh Newlin <jnewlin@purdue.edu>
2016-11-06 23:17:32 -0500

    Trying to make a recording app

--- app/src/main/java/com/example/jnew23/testapplication/MainActivity.java
+++ app/src/main/java/com/example/jnew23/testapplication/MainActivity.java
@@ -1,27 +1,88 @@
 package com.example.jnew23.testapplication;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public final static String EXTRA_MESSAGE = "com.example.jnew23.testapplication.MESSAGE";
+    private Button play, stop, record;
+    private MediaRecorder myRecorder;
+    private String outputFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        play = (Button) findViewById(R.id.play);
+        stop = (Button) findViewById(R.id.stop);
+        record = (Button) findViewById(R.id.record);
+        stop.setEnabled(false);
+        play.setEnabled(false);
 
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
+
+        myRecorder = new MediaRecorder();
+        myRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        myRecorder.setOutputFile(outputFile);
+
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    myRecorder.prepare();
+                    myRecorder.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                record.setEnabled(false);
+                stop.setEnabled(true);
+
+                Toast.makeText(getApplicationContext(), "Recording Started", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myRecorder.stop();
+                myRecorder.release();
+                myRecorder = null;
+
+                stop.setEnabled(false);
+                play.setEnabled(true);
+                record.setEnabled(true);
+
+                Toast.makeText(getApplicationContext(), "Recording Finished", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = new MediaPlayer();
+
+                try {
+                    mp.setDataSource(outputFile);
+                    mp.prepare();
+                    mp.start();
+
+                    Toast.makeText(getApplicationContext(), "Playing...", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
